abe9404515430bb72c8f687d4ee46e65b9418dd9
Yannis Mouhoun <ymouhoun@nymphes.tv>
2026-03-15 17:54:57 +0000

    File changes

---
+++ src/components/ui/shining-text.jsx
@@ -0,0 +1,21 @@
+"use client"
+
+import * as React from "react"
+import { motion } from "motion/react";
+
+export function ShiningText({ text }: { text: string }) {
+  return (
+    <motion.h1
+      className="bg-[linear-gradient(110deg,#404040,35%,#fff,50%,#404040,75%,#404040)] bg-[length:200%_100%] bg-clip-text text-base font-regular text-transparent"
+      initial={{ backgroundPosition: "200% 0" }}
+      animate={{ backgroundPosition: "-200% 0" }}
+      transition={{
+        repeat: Infinity,
+        duration: 2,
+        ease: "linear",
+      }}
+    >
+      {text}
+    </motion.h1>
+  );
+}
